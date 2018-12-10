b1a78a3c5c7b2e77db4337115844baff52140bd1
AmirulAmin <32006415+amirulaminshafiee@users.noreply.github.com>
2018-12-11 00:23:26 +0800

    Add files via upload

---
+++ add-item.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-add-item',
+  templateUrl: 'add-item.html',
+})
+export class AddItemPage {
+
+  title: string;
+  description: string;
+  constructor(public navCtrl: NavController, public view: ViewController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AddItemPage');
+  }
+
+  saveItem(){
+    let newItem = {
+      title: this.title,
+      description: this.description
+    };
+    this.view.dismiss(newItem);
+  }
+
+  close(){
+    this.view.dismiss();
+  }
+
+}
